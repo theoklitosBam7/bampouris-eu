0796cd67e11eab75810d452dfe5f571041b99070
theoklitosBam7 <th.bampouris@gmail.com>
2025-07-26 13:44:09 +0300

    feat: integrate tailwindcss, add perfectionist rules
    - Integrate TailwindCSS into Astro config
    - Add ESLint perfectionist plugin and sorting rules
    - Reformat files

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,35 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import perfectionist from "eslint-plugin-perfectionist";
 
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
+    plugins: {
+      perfectionist,
+    },
     rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+      "perfectionist/sort-array-includes": "error",
+      "perfectionist/sort-classes": "error",
+      "perfectionist/sort-decorators": "error",
+      "perfectionist/sort-enums": "error",
+      "perfectionist/sort-exports": "error",
+      "perfectionist/sort-heritage-clauses": "error",
+      "perfectionist/sort-imports": "error",
+      "perfectionist/sort-interfaces": "error",
+      "perfectionist/sort-intersection-types": "error",
+      "perfectionist/sort-jsx-props": "error",
+      "perfectionist/sort-maps": "error",
+      "perfectionist/sort-modules": "error",
+      "perfectionist/sort-named-exports": "error",
+      "perfectionist/sort-named-imports": "error",
+      "perfectionist/sort-object-types": "error",
+      "perfectionist/sort-objects": "error",
+      "perfectionist/sort-sets": "error",
+      "perfectionist/sort-switch-case": "error",
+      "perfectionist/sort-union-types": "error",
+      "perfectionist/sort-variable-declarations": "error",
     },
   },
 ];
